7cf1e9f42d390b596896bc208bf37d5dedac19f4
JD Hartley <jdhrtl@gmail.com>
2013-07-20 11:51:59 -0500

    Bug fixes with dots list and removing; check if adjacent dot; add logic for touchevents

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ var DOTS = (function()
 		flag = false,
 		color,
 		dots = [],
+		count = 0,
 
 		_init = function()
 		{
@@ -20,52 +21,96 @@ var DOTS = (function()
 				);
 			}
 
+			$('html, body').on('touchstart touchmove', function(e) { e.preventDefault() });
+
 			$('body')
-				.on('mousedown', 'li', function()
+				.on('mousedown touchstart', 'li', function()
 				{
 					flag = true;
-					color = $(this).attr('class').replace('active', '').trim();
+					color = $(this).addClass('active').attr('class').replace('active', '').trim();
 					dots.push( this );
 				})
-				.on('mousemove', 'li', function()
+				.on('mousemove touchmove', 'li', function(e)
 				{
-					// Are we touching?
+					if ( e.originalEvent.touches && e.originalEvent.touches.length )
+					{
+						e = e.originalEvent.touches[0];
+					}
+					else if ( e.originalEvent.changedTouches && e.originalEvent.changedTouches.length )
+					{
+						e = e.originalEvent.changedTouches[0];
+					}
+					var that = document.elementFromPoint(e.pageX, e.pageY);
+
+					// Are we even touching?
 					if ( ! flag )
 						return;
 
 					// Is this dot the right color?
-					if ( color !== $(this).attr('class').replace('active', '').trim() )
+					if ( color !== $(that).attr('class').replace('active', '').trim() )
 						return;
 
-					// Test if this is an adjacent dot
-					var isAdjacent = true,
-						index = $('li').index( $(this) );
+					var $lis = $('li'),
+						index = $lis.index( $(that) );
 
-					// if ( index )
+					if ( dots.length > 1 )
+					{
+						var $last = $( dots[ dots.length - 1 ] ),
+							lastIndex = $lis.index( $last );
+
+						// Test if this is an adjacent dot
+						if ( ! (
+							index === lastIndex // same dot
+								||
+							index + 6 === lastIndex // dot is to the right
+								||
+							index - 6 === lastIndex // dot is to the left
+								||
+							index + 1 === lastIndex // dot is above  TODO: check if same column too
+								||
+							index - 1 === lastIndex // dot is below  TODO: check if same column too
+						) )
+						{
+							return;
+						}
+
+						// Are we going backwards?
+						if ( index === $lis.index( $(dots[dots.length - 2]) ) )
+						{
+							$last.removeClass('active');
+							dots.pop();
+							return;
+						}
+					}
 
 					// This dot is awesome! Let's make it active
-					$(this).addClass('active');
-					dots.push( this );
+					if ( ! $(that).hasClass('active') )
+					{
+						$(that).addClass('active');
+						dots.push( that );
+					}
 				})
-				.on('mouseup', function()
+				.on('mouseup touchend', function()
 				{
 					flag = false;
 					if ( dots.length > 1 )
 					{
-						$(dots).trigger('remove');
+						$(dots).trigger('dotRemove');
 					}
 					dots = [];
+					$('li.active').removeClass('active');
 				})
-				.on('remove', 'li', function()
+				.on('dotRemove', 'li', function()
 				{
+					var $this = $(this);
 					$(this).parent().append( newDot() );
-					$(this).remove();
+					setTimeout(function() { $this.remove(); }, 600);
 				})
 
 		}
 		newDot = function()
 		{
-			return $('<li/>').addClass( colors[ Math.floor(Math.random() * 5) ] );
+			return $('<li/>').addClass( colors[ Math.floor(Math.random() * 5) ] ).attr('id', 'd' + ++count);
 		};
 
 
