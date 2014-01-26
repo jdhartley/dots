d97e764d738be97f775a5275b11e183b6d2c6e34
JD Hartley <jdhrtl@gmail.com>
2014-01-26 17:25:26 -0600

    code style updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
-var DOTS = (function()
-{
+var DOTS = (function() {
 	'use strict';
 
 	var colors = ['g','p','r','b','y'],
@@ -17,46 +16,32 @@ var DOTS = (function()
 		timeLeft,
 		timedLoop,
 
-		_init = function()
-		{
+		_init = function() {
 			$board = $('#board').empty();
 
 			$gameKeeper = $('#gameKeeper')
-				.html(function()
-				{
-					if ( gameMode === 'infinite' )
-					{
+				.html(function() {
+					if ( 'infinite' === gameMode ) {
 						return 'Moves';
-					}
-					else if ( gameMode === 'moves' )
-					{
+					} else if ( 'moves' === gameMode ) {
 						return 'Moves Left';
-					}
-					else if ( gameMode === 'timed' )
-					{
+					} else if ( 'timed' === gameMode ) {
 						return 'Time';
 					}
 				})
-				.attr('data-value', function()
-				{
-					if ( gameMode === 'infinite' )
-					{
+				.attr('data-value', function() {
+					if ( 'infinite' === gameMode ) {
 						return '0';
-					}
-					else if ( gameMode === 'moves' )
-					{
+					} else if ( 'moves' === gameMode ) {
 						return '30';
-					}
-					else if ( gameMode === 'timed' )
-					{
+					} else if ( 'timed' === gameMode ) {
 						return '60';
 					}
 				});
 
 			$score = $('#score').attr('data-value', 0);
 
-			$(window).off('resize.dots').on('resize.dots', function()
-			{
+			$(window).off('resize.dots').on('resize.dots', function() {
 				$board.css(
 					'font-size',
 					Math.min(
@@ -67,29 +52,23 @@ var DOTS = (function()
 			}).trigger('resize')[0].scrollTo(0,0);
 
 			// Make a 6x6 grid of dots!
-			var fillRow = function()
-			{
-				for ( var d = 0; d < 6; d++ )
-				{
+			var fillRow = function() {
+				for ( var d = 0; d < 6; d++ ) {
 					$(this).append( newDot() );
 				}
 			};
-			for ( var u = 0; u < 6; u++ )
-			{
+			for ( var u = 0; u < 6; u++ ) {
 				$board.append($('<ul/>').each(fillRow));
 			}
 
 			$board
 				.off('.dots')
 				.on('touchstart.dots touchmove.dots', function(e) { e.preventDefault(); })
-				.on('dblclick.dots', 'li', function()
-				{
+				.on('dblclick.dots', 'li', function() {
 					$(this).trigger('dotRemove');
 				})
-				.on('mousedown.dots touchstart.dots', 'li', function(e)
-				{
-					if ( flag === true ) // prevent multitap
-					{
+				.on('mousedown.dots touchstart.dots', 'li', function(e) {
+					if ( flag ) { // prevent multitap
 						return;
 					}
 
@@ -102,20 +81,17 @@ var DOTS = (function()
 					currentNoteIndex++;
 					_playCurrentNoteIndex();
 				})
-				.on('mousemove.dots touchmove.dots', 'li', function(e)
-				{
+				.on('mousemove.dots touchmove.dots', 'li', function(e) {
 					e = _fixTouchEvent(e);
 					var that = document.elementFromPoint(e.pageX, e.pageY);
 
 					// Are we even touching?
-					if ( ! flag )
-					{
+					if ( ! flag ) {
 						return;
 					}
 
 					// Is this dot the right color?
-					if ( color !== $(that).attr('class').replace('active', '').trim() )
-					{
+					if ( color !== $(that).attr('class').replace('active', '').trim() ) {
 						return;
 					}
 
@@ -124,8 +100,7 @@ var DOTS = (function()
 						$last = false,
 						lastIndex = -1;
 
-					if ( dots.length > 0 )
-					{
+					if ( dots.length > 0 ) {
 						$last = $( dots[ dots.length - 1 ] );
 						lastIndex = $lis.index( $last );
 
@@ -136,51 +111,42 @@ var DOTS = (function()
 							index - 6 === lastIndex || // dot is to the left
 							index + 1 === lastIndex || // dot is above  TODO: check if same column too
 							index - 1 === lastIndex // dot is below  TODO: check if same column too
-						) )
-						{
+						) ) {
 							return;
 						}
 
 						// Are we going backwards?
-						if ( index === $lis.index( $(dots[dots.length - 2]) ) )
-						{
+						if ( index === $lis.index( $(dots[dots.length - 2]) ) ) {
 							$last.removeClass('active');
 							dots.pop();
 
 							currentNoteIndex--;
 							_playCurrentNoteIndex();
 
-							if ( $board.hasClass('inSquare') && ! _activeDotsInSquare() )
-							{
+							if ( $board.hasClass('inSquare') && ! _activeDotsInSquare() ) {
 								$board.removeClass();
 							}
 							return;
 						}
 					}
 
-					if ( ! $(that).hasClass('active') || index !== lastIndex )
-					{
+					if ( ! $(that).hasClass('active') || index !== lastIndex ) {
 						// This dot is awesome! Let's make it active
 						$(that).addClass('active');
 						dots.push( that );
 						currentNoteIndex++;
 
-						if ( _activeDotsInSquare() )
-						{
+						if ( _activeDotsInSquare() ) {
 							$board.addClass('inSquare ' + color);
 							_playCurrentNoteIndex(true);
-						}
-						else
-						{
+						} else {
 							_playCurrentNoteIndex();
 						}
 					}
 				})
-				.on('mouseup.dots touchend.dots', function()
-				{
+				.on('mouseup.dots touchend.dots', function() {
 					flag = false;
-					if ( dots.length > 1 )
-					{
+					if ( dots.length > 1 ) {
 						$( _activeDotsInSquare() ? $('.' + color) : dots).trigger('dotRemove');
 						_updateGameKeeper();
 					}
@@ -189,28 +155,23 @@ var DOTS = (function()
 					$board.removeClass('inSquare');
 					currentNoteIndex = 0;
 				})
-				.on('dotRemove.dots', 'li', function()
-				{
+				.on('dotRemove.dots', 'li', function() {
 					var $this = $(this);
 					$(this).parent().append( newDot() );
 					setTimeout(function() { $this.remove(); }, 0);
 					_addPoint();
 				});
 
-			$('footer').off('click.dots').on('click.dots', 'li', function()
-			{
+			$('footer').off('click.dots').on('click.dots', 'li', function() {
 				$(this).addClass('active').siblings().removeClass('active');
 				gameMode = $(this).attr('data-value');
 				_init();
 			});
 
-			if ( gameMode === 'timed' )
-			{
+			if ( 'timed' === gameMode ) {
 				timeLeft = 60;
-				timedLoop = setInterval(function()
-				{
-					if ( --timeLeft <= 0 )
-					{
+				timedLoop = setInterval(function() {
+					if ( --timeLeft <= 0 ) {
 						clearInterval(timedLoop);
 						$board.off('.dots');
 					}
@@ -219,61 +180,46 @@ var DOTS = (function()
 				}, 1000);
 			}
 		},
-		newDot = function()
-		{
+		newDot = function() {
 			return $('<li/>').addClass( colors[ Math.floor(Math.random() * 5) ] ).attr('id', 'd' + (++count));
 		},
-		_fixTouchEvent = function(e)
-		{
-			if ( ! e.originalEvent )
-			{
+		_fixTouchEvent = function(e) {
+			if ( ! e.originalEvent ) {
 				return e;
 			}
-			if ( e.originalEvent.touches && e.originalEvent.touches.length )
-			{
+			if ( e.originalEvent.touches && e.originalEvent.touches.length ) {
 				return e.originalEvent.touches[0];
 			}
-			if ( e.originalEvent.changedTouches && e.originalEvent.changedTouches.length )
-			{
+			if ( e.originalEvent.changedTouches && e.originalEvent.changedTouches.length ) {
 				return e.originalEvent.changedTouches[0];
 			}
 			return e;
 		},
-		_activeDotsInSquare = function()
-		{
+		_activeDotsInSquare = function() {
 			var ids = $(dots).map(function() { return this.id; }),
 				itemsChecked = {};
 
-			for ( var i = 0; i < ids.length; i++ )
-			{
-				if ( Object.prototype.hasOwnProperty.call(itemsChecked, ids[i]) )
-				{
+			for ( var i = 0; i < ids.length; i++ ) {
+				if ( Object.prototype.hasOwnProperty.call(itemsChecked, ids[i]) ) {
 					return true;
 				}
 				itemsChecked[ ids[i] ] = true;
 			}
 			return false;
 		},
-		_playCurrentNoteIndex = function(inSquare)
-		{
+		_playCurrentNoteIndex = function(inSquare) {
 			// TO DO: sounds based on note
+			return inSquare;
 		},
-		_addPoint = function()
-		{
+		_addPoint = function() {
 			$score.attr('data-value', function(index, value) { return parseInt(value) + 1; });
 		},
-		_updateGameKeeper = function()
-		{
-			if ( gameMode === 'infinite' )
-			{
+		_updateGameKeeper = function() {
+			if ( 'infinite' === gameMode ) {
 				$gameKeeper.attr('data-value', function(index, value) { return parseInt(value) + 1; });
-			}
-			else if ( gameMode === 'moves' )
-			{
+			} else if ( 'moves' === gameMode ) {
 				$gameKeeper.attr('data-value', function(index, value) { return parseInt(value) - 1; });
-			}
-			else if ( gameMode === 'timed' )
-			{
+			} else if ( 'timed' === gameMode ) {
 				// handled in timedLoop
 			}
 		};
